d37de5d1f8316499ab7db6c7b2e5cb6b70563563
Ajay Chaurasiya <106958639+Ajaychaurasiya1@users.noreply.github.com>
2026-01-04 18:16:18 +0530

    Change multer upload destination to /tmp

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 const storage = multer.diskStorage({
-  destination: "uploads",
+   destination: "/tmp",
   filename: (req, file, cb) => {
     return cb(null, `${Date.now()}${file.originalname}`);
   },
